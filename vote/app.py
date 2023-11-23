4cc7a1d3c4235b898fd1e6b252458542d0610ecc
Will Patterson <50717207+wpatterson134@users.noreply.github.com>
2023-11-23 00:48:18 -0800

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "GOOD")
+option_b = os.getenv('OPTION_B', "BAD")
 hostname = socket.gethostname()
 version = 'v1'
 
