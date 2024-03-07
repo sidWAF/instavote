bcba5f242cfc596e381ac66e02e6daa2b1322e54
Will Patterson <50717207+wpatterson134@users.noreply.github.com>
2024-03-06 16:56:31 -0800

    Update StateMachine.py

--- vote/StateMachine.py
+++ vote/StateMachine.py
@@ -1,38 +1,38 @@
-import boto3
+# import boto3
 
-class StateMachine:
-    """Encapsulates Step Functions state machine actions."""
+# class StateMachine:
+#     """Encapsulates Step Functions state machine actions."""
 
-    def __init__(self, stepfunctions_client):
-        """
-        :param stepfunctions_client: A Boto3 Step Functions client.
-        """
-        self.stepfunctions_client = stepfunctions_client
+#     def __init__(self, stepfunctions_client):
+#         """
+#         :param stepfunctions_client: A Boto3 Step Functions client.
+#         """
+#         self.stepfunctions_client = stepfunctions_client
 
 
-    def start(self, state_machine_arn, run_input):
-        """
-        Start a run of a state machine with a specified input. A run is also known
-        as an "execution" in Step Functions.
+#     def start(self, state_machine_arn, run_input):
+#         """
+#         Start a run of a state machine with a specified input. A run is also known
+#         as an "execution" in Step Functions.
 
-        :param state_machine_arn: The ARN of the state machine to run.
-        :param run_input: The input to the state machine, in JSON format.
-        :return: The ARN of the run. This can be used to get information about the run,
-                 including its current status and final output.
-        """
-        try:
-            response = self.stepfunctions_client.start_execution(
-                stateMachineArn=state_machine_arn, input=run_input
-            )
-        except ClientError as err:
-            logger.error(
-                "Couldn't start state machine %s. Here's why: %s: %s",
-                state_machine_arn,
-                err.response["Error"]["Code"],
-                err.response["Error"]["Message"],
-            )
-            raise
-        else:
-            return response["executionArn"]
+#         :param state_machine_arn: The ARN of the state machine to run.
+#         :param run_input: The input to the state machine, in JSON format.
+#         :return: The ARN of the run. This can be used to get information about the run,
+#                  including its current status and final output.
+#         """
+#         try:
+#             response = self.stepfunctions_client.start_execution(
+#                 stateMachineArn=state_machine_arn, input=run_input
+#             )
+#         except ClientError as err:
+#             logger.error(
+#                 "Couldn't start state machine %s. Here's why: %s: %s",
+#                 state_machine_arn,
+#                 err.response["Error"]["Code"],
+#                 err.response["Error"]["Message"],
+#             )
+#             raise
+#         else:
+#             return response["executionArn"]
 
 
