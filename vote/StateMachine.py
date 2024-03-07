7e6456f95a21883f828a5ca2367d5daa234c399f
Will Patterson <50717207+wpatterson134@users.noreply.github.com>
2024-03-06 16:35:01 -0800

    Create StateMachine.py

---
+++ vote/StateMachine.py
@@ -0,0 +1,36 @@
+class StateMachine:
+    """Encapsulates Step Functions state machine actions."""
+
+    def __init__(self, stepfunctions_client):
+        """
+        :param stepfunctions_client: A Boto3 Step Functions client.
+        """
+        self.stepfunctions_client = stepfunctions_client
+
+
+    def start(self, state_machine_arn, run_input):
+        """
+        Start a run of a state machine with a specified input. A run is also known
+        as an "execution" in Step Functions.
+
+        :param state_machine_arn: The ARN of the state machine to run.
+        :param run_input: The input to the state machine, in JSON format.
+        :return: The ARN of the run. This can be used to get information about the run,
+                 including its current status and final output.
+        """
+        try:
+            response = self.stepfunctions_client.start_execution(
+                stateMachineArn=state_machine_arn, input=run_input
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't start state machine %s. Here's why: %s: %s",
+                state_machine_arn,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["executionArn"]
+
+
