f8cff7cfe2fbbc09104323f6df2ad394072fd10f
Will Patterson <50717207+wpatterson134@users.noreply.github.com>
2024-03-07 19:10:36 -0800

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -55,7 +55,7 @@ def hello():
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
         redis.rpush('votes', data)
         print(vote)
-        if vote == "b":
+        if vote == "b" or vote == "a":
             # trigger step-function
             print("triggering eventbridge")
             
